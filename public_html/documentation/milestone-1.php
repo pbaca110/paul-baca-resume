53179f1faef483fdd9f94278e4af5db285fd3656
Paul Baca <pbaca110@cnm.edu>
2017-04-26 09:02:58 -0600

    finished my persona,goals,audience

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: paully
- * Date: 4/26/17
- * Time: 8:33 AM
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Paul-baca-resume</title>
+	</head>
+
+	<h1>Purpose</h1>
+	<p>My purpose for this website would be to display my attributes and skills I have and will learn.</p>
+	<h1>persona</h1>
+	<p>Hi my name is Paul Baca and I am 19 years of age and im looking to create a user friendly website that displays my
+		skills and my learning experiences so that I can display websites I have made to attract clients and or jobs I am
+		looking for</p>
+	<h1>Use Case</h1>
+	<p></p>
+	<h2>Audience</h2>
+	<p>My main audience target is going to be clients or for jobs I am looking to apply in.</p>
+	<main>
+		<h1>Goals:</h1>
+		<ul>
+			<li>to acquire a internship or full time employment</li>
+			<li>to attract freelance clients
+				<ul>
+				</ul>
+
+	</main>
+
+
+
+
+
