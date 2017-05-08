baaf52fab0b6a3fae786b6f7197dbeec53fa857a
Paul Baca <pbaca110@cnm.edu>
2017-05-08 14:07:56 -0600

    working on finishing milestone 2a

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,15 +4,13 @@
 		<meta charset="utf-8"/>
 		<title>Paul-baca-resume</title>
 	</head>
-
+<body>
 	<h1>Purpose</h1>
 	<p>My purpose for this website would be to display my attributes and skills I have and will learn.</p>
 	<h1>persona</h1>
-	<p>Hi my name is Paul Baca and I am 19 years of age and im looking to create a user friendly website that displays my
-		skills and my learning experiences so that I can display websites I have made to attract clients and or jobs I am
-		looking for</p>
+	<p>Hi my name is john and I am a freelance client looking for a software developer to create a simple end usuer friendly website. </p>
 	<h1>Use Case</h1>
-	<p>at demo day I hand out my cards with my website link on it the business owner is interested goes home and logs onto his mac and searches url. He then finds my website to be user friendly easy to find contact information and great display of work I have done he then calls me to build a simple website using wordpress for his business.  </p>
+	<p>at demo day a kid named Paul Baca handed me a card that had a link to his website that had everytihng   </p>
 	<h2>Audience</h2>
 	<p>My main audience target is going to be clients or for jobs I am looking to apply in.</p>
 	<main>
@@ -20,9 +18,9 @@
 		<ul>
 			<li>to acquire a internship or full time employment</li>
 			<li>to attract freelance clients
-				<ul>
-				</ul>
 
+				</ul>
+	</body>
 	</main>
 
 
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<p>since I am making a simple site to showcase myself Its going to be very user friendly
+and I want the user to have a good understanding on the work I do and how professional I am.
+I am creating a about me to let the user know the background I come from and the experience I have. below that I will Have my current projects section showcasing some of the project I am working on currently. below that I will have past projects to showcase my websites that I have created and show off my work. And finally i will have my contact me section that shows the best way to get a hold of me.  </p>
