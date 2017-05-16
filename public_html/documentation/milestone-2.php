23bd6771a15db9d9c047d3b948efc389ace7eb87
Paul Baca <pbaca110@cnm.edu>
2017-05-16 08:00:08 -0600

    fixing my pwp

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,13 +6,13 @@
 	<body>
 		<P>On my desktop wireframe I was going for the least amount of scroll as possible so everything is in front of peoples faces and they can just click and view instead of having to scroll down on my mobile wireframe is much more scroll heavy but each section has its own full scroll so that its easy to read and to look and and touch on to view the pictures I want the contact info to be easy for them to touch so they don't have to zoom in to click it.  </P>
 	</body>
-<alt> desktop wireframe
-	<img src="images/desktop.pngggg.png" >
-</alt>
 
-	<alt>  iphone wirefram
-	<img src="images/iphonessss.%20png.png" >
-	</alt>
+	<img src="images/desktop.pngggg.png" alt="desktop.pngggg.png" >
+
+
+
+	<img src="images/iphonessss.%20png.png" alt="iphonessss.%20png.png" >
+
 </html>
 
 
