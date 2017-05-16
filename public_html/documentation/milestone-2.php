f7dc5eed95cfec67e7e90e70c518893f779b2abe
Paul Baca <pbaca110@cnm.edu>
2017-05-16 08:51:06 -0600

    working on writing my sentences

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
-		<P>On my desktop wireframe I was going for the least amount of scroll as possible so everything is in front of peoples faces and they can just click and view instead of having to scroll down on my mobile wireframe is much more scroll heavy but each section has its own full scroll so that its easy to read and to look and and touch on to view the pictures I want the contact info to be easy for them to touch so they don't have to zoom in to click it.  </P>
+		<P>On my desktop wireframe I was going for the most amount of content on the page as possible so everything is in front of peoples faces and they can just click and view instead of having to scroll down. on my mobile wireframe is much more scroll heavy but each section has its own full scroll so that its easy to read and to look and and touch on to view the pictures I want the contact info to be easy for them to touch so they don't have to zoom in to click it.  </P>
 	</body>
 
 	<img src="images/desktop.pngggg.png" alt="desktop.pngggg.png" >
