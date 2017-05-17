0834817706244269d27deff9d93906c22b2757d3
Paul Baca <pbaca110@cnm.edu>
2017-05-17 11:27:41 -0600

    finished

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,8 +7,9 @@
 		<P>On my desktop wireframe I was going for the most amount of content on the page as possible so everything is in front of peoples faces and they can just click and view instead of having to scroll down. on my mobile wireframe is much more scroll heavy but each section has its own full scroll so that its easy to read and to look and and touch on to view the pictures I want the contact info to be easy for them to touch so they don't have to zoom in to click it.  </P>
 	</body>
 
-	<img src="images/pwp.png" alt="pwp.png"
-		  <img src="images/pwp.iphone.png" alt="pwp.iphone.png"
+	<img src="images/pwp.png" alt="pwp.png">
+
+	<img src="images/pwp.iphone.png" alt="pwp.iphone.png">
 
 
 
