df786390de70ee1ce95a30073b9a6033fe340b94
Paul Baca <pbaca110@cnm.edu>
2017-05-31 15:40:49 -0600

    loaded bootstrap

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,15 @@
 	<head>
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!--- latesty compiled and minified css-->
+		<link><link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Font awesome-->
+
+
 		<title>home</title>
 	</head>
 	<body>
