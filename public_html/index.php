444a60fb264ebf410c80ee69742edd64bd3c40ff
Paul Baca <pbaca110@cnm.edu>
2017-05-31 14:21:57 -0600

    fixing my css

---
+++ public_html/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+
+	<head>
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+		<meta charset="utf-8">
+		<title>home</title>
+	</head>
+	<body>
+		<h1>ABOUT ME</h1>
+		<p>Hi I am Paul Baca and I am a web designer who enjoys working on back end development and likes to develop font end as well. <p>I took the Deep Dive Coding Bootcamp in April of 2017, below you will see some of the work I have done and am working on currently. <p>Please feel free to contact me with any questions you may have. </p>
+		<h1>CURRENT PROJECTS</h1>
+		<h2> OUR VIBE</h2>
+		<a href="https://github.com/btrabaudo/our-vibe/tree/master/epic" target="_blank"><img  class= "resize" src="images/our.png"></a>
+
+		<h1>PAST PROJECTS</h1>
+	</body>
+	<footer>
+		<h1>CONTACT ME</h1>
+	</footer>
+</html>
